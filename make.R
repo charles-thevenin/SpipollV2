7121031093f1cb9a43740aa4d7197ff6351e3f66
Charles THÉVENIN <charles.thevenin@mnhn.fr>
2022-11-04 16:29:24 +0100

    Init repo

---
+++ make.R
@@ -0,0 +1,31 @@
+#' SpipollV2: A Research Compendium
+#' 
+#' @description 
+#' A paragraph providing a full description of the project and describing each 
+#' step of the workflow.
+#' 
+#' @author Charles Thévenin \email{thevenin.charles@gmail.com}
+#' 
+#' @date 2022/11/04
+
+
+
+## Install Dependencies (listed in DESCRIPTION) ----
+
+devtools::install_deps(upgrade = "never")
+
+
+## Load Project Addins (R Functions and Packages) ----
+
+devtools::load_all(here::here())
+
+
+## Global Variables ----
+
+# You can list global variables here (or in a separate R script)
+
+
+## Run Project ----
+
+# List all R scripts in a sequential order and using the following form:
+# source(here::here("analyses", "script_X.R"))
